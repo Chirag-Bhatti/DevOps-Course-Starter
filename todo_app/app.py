acde9e73f05fdf08615601a22985f958e5481c53
Chirag Bhatti <Chirag.Bhatti@softwire.com>
2022-07-18 12:55:34 +0100

    feat: E1: add functionality to create new to-do items and show them

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import add_item, get_items
 
 from todo_app.flask_config import Config
 
@@ -11,3 +11,9 @@ app.config.from_object(Config())
 def index():
     toDoItems = get_items()
     return render_template('index.html', toDoItems = toDoItems)
+
+@app.route('/add-todo-item', methods=['POST'])
+def addToDoItem():
+    title = request.form.get('title')
+    add_item(title)
+    return redirect('/')
